eb4bbcfa2ae0b268ed2d5bb3887df3d115daf660
Stridsvagn69420 <Stridsvagn69420@users.noreply.github.com>
2021-11-18 20:03:53 +0100

    Fix errors in build script (why does VS 2022 not have IntelliSense for PowerShell?)

--- buildAll.ps1
+++ buildAll.ps1
@@ -5,10 +5,10 @@ $allPlatforms = @(
 
 	'linux-arm64',
 	'linux-arm',
-	'linux-x64',
+	'linux-x64'
 );
 
-for ($platform in $allPlatforms) {
+foreach ($platform in $allPlatforms) {
 	dotnet publish -c Release -r $platform --self-contained true -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -p:PublishTrimmed=True -p:TrimMode=CopyUsed
 }
 dotnet publish --no-self-contained -c Release
